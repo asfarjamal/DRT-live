33d9413c279e2e87583e1fc1b2d23b81a9d60337
asfarjamal <101565049+asfarjamal@users.noreply.github.com>
2024-07-21 23:44:53 +0530

    Reorganized folder structure by moving frontend inside backend

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,15 @@ const teamRoutes = require('./routes/teamRoutes');
 const contactRoutes = require('./routes/contact');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
+//const { fileURLToPath } = require('url');
+
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
+
+//  __filename = fileURLToPath(import.meta.url);
+//  __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -16,16 +22,20 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 // Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, Drift Racing Team!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, Drift Racing Team!');
+// });
 
 // Routes
 app.use('/api/team', teamRoutes);
 app.use('/api/contact', contactRoutes);
 
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log(`Server running on port 5000`);
 });
